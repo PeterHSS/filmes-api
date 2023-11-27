6873e909daf36bd8f2ab8bb70a4d831aeb42e9c6
PeterHSS <pedro.hennrique.ismart@gmail.com>
2023-11-26 23:13:15 -0300

    projeto filmes api - alura cursos

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,111 @@
+﻿using AutoMapper;
+using FilmesAPI.Data;
+using FilmesAPI.Data.Dtos;
+using FilmesAPI.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Immutable;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+
+        private FilmeContext _context;
+        private IMapper _mapper;
+
+        public FilmeController(FilmeContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Adiciona um filme ao banco de dados
+        /// </summary>
+        /// <param name="filmeDto">Objeto com os campos necessários para criação de um filme</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="201">Caso inserção seja feita com sucesso</response>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public IActionResult AdicionaFilme([FromBody] CreateFilmeDto createFilmeDto)
+        {
+            Filme filme = _mapper.Map<Filme>(createFilmeDto);
+            _context.Filmes.Add(filme);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(RecuperaFilmePorId), new { id = filme.Id }, filme);
+        }
+
+        [HttpGet]
+        public IEnumerable<ReadFilmeDto> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
+        {
+            return _mapper.Map<List<ReadFilmeDto>>(_context.Filmes.Skip(skip).Take(take));
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmePorId(int id)
+        {
+            var filme = _context.Filmes.FirstOrDefault(filmes => filmes.Id == id);
+
+            if (filme == null)
+                return NotFound();
+
+            var readFilmeDto = _mapper.Map<ReadFilmeDto>(filme);    
+
+            return Ok(readFilmeDto);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult AtualizaFilme(int id, [FromBody] UpdateFilmeDto updateFilmeDto)
+        {
+            var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+
+            if (filme == null)
+                return NotFound();
+
+            _mapper.Map(updateFilmeDto, filme);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult AtualizaFilmeParcial(int id, JsonPatchDocument<UpdateFilmeDto> jsonPatch)
+        {
+            var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+
+            if (filme == null)
+                return NotFound();
+
+            var filmeParaAtualizar = _mapper.Map<UpdateFilmeDto>(filme);
+
+            jsonPatch.ApplyTo(filmeParaAtualizar, ModelState);
+
+            if(!TryValidateModel(filmeParaAtualizar))
+                return ValidationProblem(ModelState);
+
+            _mapper.Map(filmeParaAtualizar, filme);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        
+        public IActionResult DeletaFilme(int id)
+        {
+            var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+
+            if (filme == null)
+                return NotFound();
+
+            _context.Remove(filme);
+            
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
---
+++ FilmesAPI/Data/Dtos/ReadFilmeDto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos
+{
+    public class ReadFilmeDto
+    {
+        public string? Titulo { get; set; }
+
+        public string? Genero { get; set; }
+
+        public int Duracao { get; set; }
+
+        public DateTime HoraDaConsulta { get; set; } = DateTime.Now;    
+    }
+}
